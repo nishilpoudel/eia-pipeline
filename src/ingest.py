3a73d12e2b2a7c5f8284c1858fd2330e460a80a9
Nishil <nishilpoudel11@gmail.com>
2026-06-01 03:55:31 +0000

    Add EIA backfill ingestion script

---
+++ src/ingest.py
@@ -0,0 +1,64 @@
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+import os
+from pathlib import Path
+
+
+
+load_dotenv()
+
+EIA_API_KEY = os.getenv("EIA_API_KEY")
+BASE_DIR = Path(__file__).parent.parent
+
+
+
+def fetch_bulk():
+    
+    offset = 5000; 
+    total = 0
+    total_response = {}
+    
+    url = f'https://api.eia.gov/v2/electricity/rto/region-data/data/?api_key={EIA_API_KEY}&frequency=hourly&data[0]=value&facets[respondent][]=ERCO&facets[type][]=D&start=2024-05-30T00&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000'
+    try: 
+        request = requests.get(url=url)
+        response = request.json()
+    except :
+        print("Error")
+        return
+
+    total = int(response['response']['total'])
+    total_response = response['response']['data']
+    
+
+    while offset < total:
+        url = f'https://api.eia.gov/v2/electricity/rto/region-data/data/?api_key={EIA_API_KEY}&frequency=hourly&data[0]=value&facets[respondent][]=ERCO&facets[type][]=D&start=2024-05-30T00&sort[0][column]=period&sort[0][direction]=desc&offset={offset}&length=5000'
+        try: 
+            request = requests.get(url=url)
+            response = request.json()
+        except: 
+            print("There was an error fetching the data")
+        else:
+            'The data was read into memory'
+
+        data = response['response']['data']
+
+        total_response += data
+
+        offset+=5000
+        
+    return total_response
+
+
+bulk_data = fetch_bulk()
+
+df = pd.DataFrame(bulk_data)
+filtered_df = df[['period', 'value']]
+filtered_df.to_csv(BASE_DIR/"data/raw/ercot_backfill.csv", index=False)
+
+#Test, this is from nano 
+
+
+
+
+
