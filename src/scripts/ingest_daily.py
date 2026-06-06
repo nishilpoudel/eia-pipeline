6072dd4bc22e6964cb2ca4024ca0b863dafbeda3
Nishil <nishilpoudel11@gmail.com>
2026-06-06 21:39:30 +0000

    Refactor: Add empty response gaurd and more error handling

--- src/scripts/ingest_daily.py
+++ src/scripts/ingest_daily.py
@@ -7,10 +7,10 @@ import os
 from dotenv import load_dotenv
 import csv
 from pathlib import Path
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta, timezone
 import pandas as pd 
 from collections import deque
+import sys
 
 load_dotenv() 
 
@@ -19,43 +19,55 @@ EIA_API_KEY = os.getenv("EIA_API_KEY")
 # .parent will go one level higher
 BASE_DIR = Path(__file__).parent.parent.parent
 FILE_PATH = BASE_DIR/"data/raw/ercot_demand.csv"
+FORMAT_PATTERN = "%Y-%m-%dT%H"
 
 
-with open(FILE_PATH, mode="r", newline="") as f:
-    reader = csv.reader(f)
-    next(reader, None)
-    # deque(d.append(next(reader))) all in one line 
-    last_row = deque(reader, maxlen=1)
-    previous_time_stamp = last_row[0][0]
+#Read latest timestamp from csv and return next timestamp to fetch
+def get_next_timestamp(file_path):
 
-format_pattern="%Y-%m-%dT%H"
-dt_object = datetime.strptime(previous_time_stamp, format_pattern)
-updated_timestamp = dt_object + timedelta(hours=1)
-time_stamp = updated_timestamp.strftime(format_pattern)
+    with open(file_path, mode="r", newline="") as f:
+        reader = csv.reader(f)
+        next(reader, None)
+        # deque(d.append(next(reader))) all in one line 
+        last_row = deque(reader, maxlen=1)
+        latest_time_stamp = last_row[0][0]
 
+    dt_object = datetime.strptime(latest_time_stamp, FORMAT_PATTERN)
+    next_timestamp = dt_object + timedelta(hours=1)
+    return next_timestamp.strftime(FORMAT_PATTERN)
 
-
-def fetch_data():
+    
+#Fetch ercot demand data
+def fetch_data(time_stamp):
     url = f"https://api.eia.gov/v2/electricity/rto/region-data/data/?api_key={EIA_API_KEY}&frequency=hourly&data[0]=value&facets[respondent][]=ERCO&facets[type][]=D&start={time_stamp}&sort[0][column]=period&sort[0][direction]=asc&offset=0&length=5000"
 
     try:
-        request = requests.get(url=url)
-        response = request.json()
-        return response
+        response = requests.get(url=url)
+        return response.json()
     except Exception as e:
         print(e)
         raise RuntimeError(f"Failed API call: {e}") from e
     
-data = fetch_data()
 
-filtered_data = data['response']['data']
+#Filter for time and demand(value) and append to csv
+def process_and_save(data, file_path):
+    
+    filtered_data = data['response']['data']
 
-df = pd.DataFrame(filtered_data)
-filtered_df = df[['period','value']]
-filtered_df.to_csv(FILE_PATH, mode="a", index=False, header=False)
+    if not filtered_data:
+        print(f"[{datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S UTC')}] No new data available. Exiting!")
+        sys.exit(0)
+    
+    df = pd.DataFrame(filtered_data)
+    filtered_df = df[['period','value']]
+    filtered_df.to_csv(file_path,mode='a',index=False, header=False)
+    print(f"[{datetime.now(timezone.utc).strftime('%Y-%m-%d %H:%M:%S UTC')}] Appended {len(filtered_df)} rows!" )
 
+def main():
+    time_stamp = get_next_timestamp(FILE_PATH)
+    data = fetch_data(time_stamp)
+    process_and_save(data, FILE_PATH)
+    
 
-
-        
-
-
+if __name__ == "__main__":
+    main()
